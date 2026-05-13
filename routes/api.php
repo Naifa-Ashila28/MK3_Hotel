3d90572aaae7c35c13a7cdea2e9f38370dfc1830
Amaa-aja <rajafidhiazka@gmail.com>
2026-05-13 13:52:56 +0700

    ngubah routes

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\HotelController;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ReviewController; // <-- TAMBAHAN: Import buat fitur review
 
 // 1. Membuat pemesanan hotel
 Route::post('/booking', [BookingController::class, 'store']);
@@ -29,11 +30,22 @@ Route::post('/payment', [PaymentController::class, 'pay']);
 Route::get('/hotels/{id}', [HotelController::class, 'show']);
 
 // 7. Untuk Lokasi
-Route::post('/user/location', [UserController::class, 'storeLocation']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/user/location', [UserController::class, 'storeLocation']);
+});
 
 // 6. Menampilkan detail hotel berdasarkan ID (Spesifik satu hotel)
 Route::get('/hotels/{id}', [HotelController::class, 'show']);
 
-// 7. INI YANG BARU: Menampilkan daftar SEMUA hotel
-// Baris ini yang bikin http://127.0.0.1:8000/api/hotels kamu nggak error 404 lagi
+// 7. Menampilkan daftar SEMUA hotel
 Route::get('/hotels', [HotelController::class, 'index']);
+
+// --- FITUR BARU: REVIEW & RATING ---
+// Untuk melihat semua ulasan yang masuk
+Route::get('/review', [ReviewController::class, 'index']); 
+
+// Untuk mengirim ulasan baru dari aplikasi Android
+Route::post('/review', [ReviewController::class, 'store']);
+
+// untuk register
+Route::post('/register', [AuthController::class, 'register']);
